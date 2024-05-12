bc4d8852de68c6264bffce40fbd7fc932ca89ac8
Khalid Obaide <sayed.khalid.obaide@gmail.com>
2024-05-12 20:14:13 +0300

    Implement texture loading

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   const int CELL_SIZE = 100;
-  GameEngine gameEngine = GameEngine("Chess", {CELL_SIZE * 8, CELL_SIZE * 80});
+  GameEngine gameEngine = GameEngine("Chess", {CELL_SIZE * 8, CELL_SIZE * 8});
   GameManager gameManager = GameManager(&gameEngine, CELL_SIZE);
   gameEngine.gameLoop();
   return 0;
